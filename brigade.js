59a00072429e6e79e40d24a3a7cb500880ec9bd7
Mayur Kumar <mayur.kumar@lapr200.successivetech.com>
2020-01-16 11:37:48 -0300

    build

--- brigade.js
+++ brigade.js
@@ -4,12 +4,15 @@ const { events, Job } = require("brigadier");
 events.on("push", () => {
   var job = new Job("dockerbuild", "docker:dind");
   job.privileged = true;
-  
+  docker.env = {
+  DOCKER_DRIVER: driver overlay
+  }  
 
   job.tasks = [
     "echo Hello",
     "echo World",
-    "dockerd &",
+    "dockerd-entrypoint.sh &",
+   // "dockerd &",
     "sleep 20",
     "cd /src/image-processing",
     "docker build -t mayursuccessive/packageimage:latest .",
