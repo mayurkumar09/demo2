6e585d457fc8f004e7c6384e69d820662e0b189b
Mayur Kumar <mayur.kumar@lapr200.successivetech.com>
2020-01-16 16:55:34 -0300

    new try

--- brigade.js
+++ brigade.js
@@ -4,23 +4,15 @@ const { events, Job } = require("brigadier");
 events.on("push", () => {
   var job = new Job("dockerbuild", "kshitizsh12/base:v1");
   job.privileged = true;
-//  job.storage.enabled = true;
-  job.env = {
-  DOCKER_DRIVER: "overlay"
-  }
-
   job.tasks = [
-    "echo Hello",
-    "echo World",
-    "dockerd-entrypoint.sh &",
-  //  "export DOCKERD_PID=$!",
-  //  `printf "waiting for docker daemon"; while ! docker info >/dev/null 2>&1; do printf .; sleep 1; done; echo`,
-
-  //  "dockerd &",
-    "sleep 20",
+    "dockerd &",
+    "sleep 5",
     "cd /src/image-processing",
+    "ls",
     "docker login -u mayursuccessive -p Successive@123",
+    "echo docker login done",
     "docker build -t mayursuccessive/packageimage:latest .",
+    "echo docker build done",
     "docker images",
     "docker ps"
 
@@ -29,3 +21,13 @@ events.on("push", () => {
 
   job.run();
 });
+
+
+//  job.storage.enabled = true;
+//job.env = {
+//  DOCKER_DRIVER: "overlay"
+//  }
+
+// "dockerd-entrypoint.sh &",
+  //  "export DOCKERD_PID=$!",
+  //  `printf "waiting for docker daemon"; while ! docker info >/dev/null 2>&1; do printf .; sleep 1; done; echo`,
