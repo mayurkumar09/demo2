eb89a29d398d35dafe291a5dd06913c5eff0c2ea
Mayur Kumar <mayur.kumar@lapr200.successivetech.com>
2020-01-16 11:52:34 -0300

    nn

--- brigade.js
+++ brigade.js
@@ -1,5 +1,5 @@
 
-var driver = project.secrets.DOCKER_DRIVER || "overlay"
+var driver = mayurkumar09/demo2.secrets.DOCKER_DRIVER || "overlay"
 const { events, Job } = require("brigadier");
 events.on("push", () => {
   var job = new Job("dockerbuild", "docker:dind");
