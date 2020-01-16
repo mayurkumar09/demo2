4a8bdc18df071c0035e08e5f9c3dabd5a467f5fc
Mayur Kumar <mayur.kumar@lapr200.successivetech.com>
2020-01-16 15:02:36 -0300

    ghsjd

--- brigade.js
+++ brigade.js
@@ -2,7 +2,7 @@
 //var driver = project.secrets.DOCKER_DRIVER || "overlay"
 const { events, Job } = require("brigadier");
 events.on("push", () => {
-  var job = new Job("job", "docker:dind");
+  var job = new Job("dockerbuild", "docker:dind");
   job.privileged = true;
 //  job.storage.enabled = true;
   job.env = {
@@ -10,11 +10,11 @@ events.on("push", () => {
   }
 
   job.tasks = [
-   // "echo Hello",
-  //  "echo World",
- //   "dockerd-entrypoint.sh &",
-    "dockerd &",
-    "sleep 5",
+    "echo Hello",
+    "echo World",
+    "dockerd-entrypoint.sh &",
+  //  "dockerd &",
+    "sleep 20",
     "cd /src/image-processing",
     "docker login -u mayursuccessive -p Successive@123",
     "docker build -t mayursuccessive/packageimage:latest .",
