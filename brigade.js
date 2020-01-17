d673d1396ee2c98f9d15e6e5be4983a160374b42
Mayur Kumar <mayur.kumar@lapr200.successivetech.com>
2020-01-17 17:28:20 -0300

    sd

--- brigade.js
+++ brigade.js
@@ -1,34 +1,37 @@
- //var driver = project.secrets.DOCKER_DRIVER || "overlay"
+//var driver = project.secrets.DOCKER_DRIVER || "overlay"
 const { events, Job } = require("brigadier");
 events.on("push", () => {
-  var job = new Job("build", "ubuntu:latest");
+  var job = new Job("build", "docker:dind");
   job.privileged = true;
-//  job.env = {
-//  DOCKER_DRIVER: "overlay"
-//  }
-//  job.allowHostMounts = true;
+  job.env = {
+    DOCKER_DRIVER: "overlay"
+  }
+  //  job.allowHostMounts = true;
   job.tasks = [
-   //    "dockerd-entrypoint.sh &",
-   "su ",
-   "echo 'u r in root'",
-   "apt-get update",
-   "apt-get install -y docker",
-   "echo docker installed",
-   "./etc/init.d/docker start",
-   "./usr/bin/docker restart",
-   "sleep 10",
-//    "cd /etc/init.d/",
-//    "ls -l",
-    "cd /src/image-processing",
-    "ls -l",
-    "docker login -u mayursuccessive -p Successive@123",
-    "echo docker login done",
-    //"docker build -t mayursuccessive/packageimage:latest .",
-    //"echo docker build done",
-    "docker images",
-    "docker ps"
+    "dockerd-entrypoint.sh &",
+    "sleep 30",
+    "docker --help",
+    // "echo 'u r in root'",
+    // "apt-get update",
+    // "apt-get install -y docker",
+    // "echo docker installed",
+    // "cd /etc/init.d/",
+    // "ls -l",
+    // //   "./etc/init.d/docker start",
+    // "./usr/bin/docker start",
+    // "sleep 10",
+    // //    "cd /etc/init.d/",
+    // //    "ls -l",
+    // "cd /src/image-processing",
+    // "ls -l",
+    // "docker login -u mayursuccessive -p Successive@123",
+    // "echo docker login done",
+    // //"docker build -t mayursuccessive/packageimage:latest .",
+    // //"echo docker build done",
+    // "docker images",
+    // "docker ps"
 
-];
+  ];
 
 
   job.run();
