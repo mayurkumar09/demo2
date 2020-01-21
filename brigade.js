3436712882b90f077b36321ffbac700a3bcf1c63
Mayur Kumar <mayur.kumar@lapr200.successivetech.com>
2020-01-21 13:30:16 +0530

    asds

--- brigade.js
+++ brigade.js
@@ -1,50 +1,13 @@
-//var driver = project.secrets.DOCKER_DRIVER || "overlay"
+
 const { events, Job } = require("brigadier");
 events.on("push", () => {
   var job = new Job("buildfinal", "docker:dind");
   job.privileged = true;
-//  job.storage.enabled = true;
-  job.env = {
-    DOCKER_DRIVER: "overlay",
-  }
-  //  job.allowHostMounts = true;
   job.tasks = [
       "cd /src",
       "ls -l",
       "dockerd-entrypoint.sh &",
       "sleep 20",
-      "docker ps"
-    // "echo 'u r in root'",
-    // "apt-get update",
-    // "apt-get install -y docker",
-    // "echo docker installed",
-    // "cd /etc/init.d/",
-    // "ls -l",
-    // //   "./etc/init.d/docker start",
-    // "./usr/bin/docker start",
-    // "sleep 10",
-    // //    "cd /etc/init.d/",
-    // //    "ls -l",
-    // "cd /src/image-processing",
-    // "ls -l",
-    // "docker login -u mayursuccessive -p Successive@123",
-    // "echo docker login done",
-    // //"docker build -t mayursuccessive/packageimage:latest .",
-    // //"echo docker build done",
-    // "docker images",
-    // "docker ps"
-
   ];
-
-
   job.run();
 });
-
-//  job.storage.enabled = true;
-//job.env = {
-//  DOCKER_DRIVER: "overlay"
-//  }
-
-// "dockerd-entrypoint.sh &",
-  //  "export DOCKERD_PID=$!",
-  //  `printf "waiting for docker daemon"; while ! docker info >/dev/null 2>&1; do printf .; sleep 1; done; echo`,
