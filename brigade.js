86eb496a30814b0d3e692e4183785cbcddb041a8
Mayur Kumar <mayur.kumar@lapr200.successivetech.com>
2020-01-22 14:55:00 +0530

    asdas

--- brigade.js
+++ brigade.js
@@ -1,15 +1,15 @@
-
 const { events, Job } = require("brigadier");
 events.on("push", () => {
   var job = new Job("buildfinal", "docker:dind");
   job.privileged = true;
-  
   job.tasks = [
       "dockerd &",
-      "sleep 20",
       "cd /src",
       "ls -l",
+      "sleep 10",
+      "docker info",
       "docker ps"
   ];
+
   job.run();
 });
